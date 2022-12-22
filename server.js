4423fe1bd0a9dcdcb5cf73c08f81195372ebe0e3
Thomas Lawlor <thomaslawlor@Thomass-MacBook-Pro.local>
2022-12-22 13:42:20 -0500

    final build v2

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use('/api/community', require('./routes/api/community'))
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX requests
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
 
 // Configure to use port 3001 instead of 3000 during
